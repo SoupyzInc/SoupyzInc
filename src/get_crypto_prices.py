d02ea0783537d209187c64aff53ee72b90342a5b
Matthew Okashita <okashita.matthew@gmail.com>
2021-06-20 08:19:38 -0400

    Query API less
    Only querying the API twice presents less opportunities for the API to return an error. Querying the data before calculations, also make data more accurate to the acclaimed timestamp.

--- src/get_crypto_prices.py
+++ src/get_crypto_prices.py
@@ -21,42 +21,47 @@ import krakenex
 
 if __name__ == "__main__":
     k = krakenex.API()
+
     # Assert that the Kraken REST API is functional before pulling data.
     if k.query_public('SystemStatus')['result']['status'] == "online":
         # Get timestamp
         timestamp = k.query_public('Time')['result']['rfc1123']
 
-        # Get BTC prices
-        btc_oldest = float(k.query_public('OHLC', {'pair': 'XBTUSD', 'interval': '60'})['result']['XXBTZUSD'][0][4])
-        btc_latest = float(k.query_public('OHLC', {'pair': 'XBTUSD', 'interval': '60'})['result']['XXBTZUSD'][-1][4])
+        # Get data first
+        btc_data = k.query_public('OHLC', {'pair': 'XBTUSD', 'interval': '60'})['result']['XXBTZUSD']
+        ltc_data = k.query_public('OHLC', {'pair': 'XLTCZUSD', 'interval': '60'})['result']['XLTCZUSD']
 
-        # Get LTC Prices
-        ltc_oldest = float(k.query_public('OHLC', {'pair': 'XLTCZUSD', 'interval': '60'})['result']['XLTCZUSD'][0][4])
-        ltc_latest = float(k.query_public('OHLC', {'pair': 'XLTCZUSD', 'interval': '60'})['result']['XLTCZUSD'][-1][4])
+        # Get BTC prices
+        btc_oldest = float(btc_data[0][4])
+        btc_latest = float(btc_data[-1][4])
 
         # Calculate BTC data
         btc_percent = round((btc_latest - btc_oldest) / btc_oldest, 2)
 
+        # Build BTC string
         btc_arrow = '▼'
         if btc_percent > 0:
             btc_arrow = '▲'
-        
-        # Build BTC string
+            
         btc_string = 'BTC: ${0} {1} {2}%'.format(btc_latest, btc_arrow, btc_percent)
 
+        # Get LTC Prices
+        ltc_oldest = float(ltc_data[0][4])
+        ltc_latest = float(ltc_data[-1][4])
+        
         # Calculate LTC data
         ltc_percent = round((ltc_latest - ltc_oldest) / ltc_oldest, 2)
 
+        # Build LTC string
         ltc_arrow = '▼'
         if ltc_percent > 0:
             ltc_arrow = '▲'
         
-        # Build LTC string
         ltc_string = 'LTC: ${0} {1} {2}%'.format(ltc_latest, ltc_arrow, ltc_percent)
 
         # Build final string
         html = '```java\n'
-        html += '| {0} | {1} | As of {2}, from the Kraken REST API. |'.format(btc_string, ltc_string, timestamp)
+        html += '| {0} | {1} | As of {2}, from Kraken.'.format(btc_string, ltc_string, timestamp)
         html +='\n```'
 
         assert(len(sys.argv) == 4)
@@ -69,3 +74,4 @@ if __name__ == "__main__":
 
         with open(readmePath, "w") as readme:
             readme.write(newContent)
+            
