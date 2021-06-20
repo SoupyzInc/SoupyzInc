a19eba11c53ca2601c761d555f0a0f428a7acfa3
Matthew Okashita <okashita.matthew@gmail.com>
2021-06-20 03:53:30 -0400

    Removed ETH data
    The ETH data did not fit on the page, so it was removed. Opted instead for clarification of the crypto prices coming from Kraken.

--- src/get_crypto_prices.py
+++ src/get_crypto_prices.py
@@ -18,10 +18,6 @@ if __name__ == "__main__":
         ltc_oldest = float(k.query_public('OHLC', {'pair': 'XLTCZUSD', 'interval': '60'})['result']['XLTCZUSD'][0][4])
         ltc_latest = float(k.query_public('OHLC', {'pair': 'XLTCZUSD', 'interval': '60'})['result']['XLTCZUSD'][-1][4])
 
-        # Get ETH Prices
-        eth_oldest = float(k.query_public('OHLC', {'pair': 'XETHZUSD', 'interval': '60'})['result']['XETHZUSD'][0][4])
-        eth_latest = float(k.query_public('OHLC', {'pair': 'XETHZUSD', 'interval': '60'})['result']['XETHZUSD'][-1][4])
-
         # Calculate BTC data
         btc_percent = round((btc_latest - btc_oldest) / btc_oldest, 2)
 
@@ -42,19 +38,9 @@ if __name__ == "__main__":
         # Build LTC string
         ltc_string = 'LTC: ${0} {1} {2}%'.format(ltc_latest, ltc_arrow, ltc_percent)
 
-        # Calculate ETH data
-        eth_percent = round((eth_latest - eth_oldest) / eth_oldest, 2)
-
-        eth_arrow = '▼'
-        if eth_percent > 0:
-            eth_arrow = '▲'
-        
-        # Build ETH string
-        eth_string = 'BTC: ${0} {1} {2}%'.format(eth_latest, eth_arrow, eth_percent)
-
         # Build final string
         html = '```java\n'
-        html += '| {0} | {1} | {2} | As of {3} |'.format(btc_string, ltc_string, eth_string, timestamp)
+        html += '| {0} | {1} | As of {2}, from Kraken.'.format(btc_string, ltc_string, timestamp)
         html +='\n```'
 
         assert(len(sys.argv) == 4)
